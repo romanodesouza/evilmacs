ed719f3d0cc82b940e3400ad00b795d4ab23407c
Romano Augusto <romanoaugusto88@gmail.com>
2014-09-18 15:39:50 -0300

    Faster movements across windows on evil-mode-normal-state

--- config/init-evil-mode.el
+++ config/init-evil-mode.el
@@ -7,5 +7,10 @@
 (evil-mode t)
 ; Enable evil-nerd-commenter
 (evilnc-default-hotkeys)
+; Key bindings
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-prev)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-next)
 
 (provide 'init-evil-mode)
