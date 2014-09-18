1d8fc23883fd7dcaf05ee3f001653c4e0177ddca
Romano Augusto <romanoaugusto88@gmail.com>
2014-09-18 14:47:54 -0300

    Refresh the package list only once per session

--- config/init-elpa.el
+++ config/init-elpa.el
@@ -3,12 +3,15 @@
   ("melpa" . "http://melpa.milkbox.net/packages/")
 ))
 (setq package-enable-at-startup nil)
+(defvar packages-refreshed nil)
 (package-initialize)
 
 (defun require-package (package)
   "Install given PACKAGE."
   (unless (package-installed-p package)
-    (package-refresh-contents)
+    (unless packages-refreshed
+      (setq packages-refreshed t)
+      (package-refresh-contents))
     (package-install package)))
 
 (provide 'init-elpa)
