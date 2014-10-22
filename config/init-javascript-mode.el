ebe0b22176471650480ebbf09166f1790c359dc9
Romano Augusto <romanoaugusto88@gmail.com>
2014-10-22 14:47:02 -0200

    Replace js3-mode in favor to js2-mode

--- config/init-javascript-mode.el
+++ config/init-javascript-mode.el
@@ -1,22 +1,23 @@
-(require-package 'js3-mode)
+(require-package 'js2-mode)
 (require-package 'tern)
 (require-package 'company-tern)
 (require-package 'web-beautify)
 
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
-(add-hook 'js3-mode-hook (lambda ()
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook (lambda ()
                            (tern-mode t)
+                           (js2-imenu-extras-mode)
                            (add-hook 'before-save-hook 'web-beautify-js-buffer t t)
                            (add-to-list 'company-backends 'company-tern)
 
-                           (setq-default js3-global-externs
+                           (setq-default js2-global-externs
                                          '("module" "require" "__dirname" "process" "console" "define"
                                            "JSON" "$" "_" "Backbone" "buster" "sinon" "moment" "_gaq"
                                            "Zenbox" "Mousetrap" "Comoyo"))
-                           (setq-default js3-indent-tabs-mode t)
-                           (setq-default js3-indent-level 4)
-                           (setq-default js3-auto-indent-p t)         ; it's nice for commas to right themselves.
-                           (setq-default js3-enter-indents-newline t) ; don't need to push tab before typing
-                           (setq-default js3-indent-on-enter-key t)))
+                           (setq-default js2-indent-tabs-mode t)
+                           (setq-default js2-indent-level 4)
+                           (setq-default js2-auto-indent-p t)         ; it's nice for commas to right themselves.
+                           (setq-default js2-enter-indents-newline t) ; don't need to push tab before typing
+                           (setq-default js2-indent-on-enter-key t)))
 
 (provide 'init-javascript-mode)
