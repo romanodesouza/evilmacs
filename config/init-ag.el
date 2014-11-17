3289ba44b4c8b01c32a05dab51da61a33a2a7bf4
Romano Augusto <romanoaugusto88@gmail.com>
2014-11-17 14:34:24 -0200

    Ag-mode: Fix "k" key for evil-mode

--- config/init-ag.el
+++ config/init-ag.el
@@ -3,4 +3,8 @@
 (setq ag-highlight-search t)
 (setq ag-reuse-buffers 't)
 
+; Fix "k" key for evil-mode
+(add-hook 'ag-mode-hook (lambda()
+                          (define-key ag-mode-map (kbd "k") nil)))
+
 (provide 'init-ag)
