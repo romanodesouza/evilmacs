7244f66fdb20a2dd7288b0c28a5447513e7b0db3
Romano Augusto <romanoaugusto88@gmail.com>
2014-12-25 16:27:19 -0200

    evil-jumper and evil-visualstar

--- config/init-evil-mode.el
+++ config/init-evil-mode.el
@@ -1,7 +1,7 @@
 (require-package 'evil)
 (require-package 'evil-nerd-commenter)
 (require-package 'evil-jumper)
-(require 'evil-jumper)
+(require-package 'evil-visualstar)
 
 ; Config
 (setq evil-search-module 'evil-search)
@@ -9,5 +9,8 @@
 (evil-mode t)
 ; Enable evil-nerd-commenter
 (evilnc-default-hotkeys)
+
+(global-evil-jumper-mode t)
+(global-evil-visualstar-mode t)
 
 (provide 'init-evil-mode)
