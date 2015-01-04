8f7cd911646e94511e1756e8baf0a05126ecc601
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-04 16:23:24 -0200

    Removes web-beautify after saving html/css files

--- config/init-web-mode.el
+++ config/init-web-mode.el
@@ -17,13 +17,4 @@
 (defadvice sgml-close-tag (after reindent activate)
            (indent-region (point-min) (point-max)))
 
-(eval-after-load 'sgml-mode
-                 '(add-hook 'html-mode-hook
-                            (lambda ()
-                              (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
-(eval-after-load 'css-mode
-                 '(add-hook 'css-mode-hook
-                            (lambda ()
-                              (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
-
 (provide 'init-web-mode)
