c8db4830d12fd8f300f65cf803ffa8eb1745dc24
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-12 02:08:18 -0200

    Keeps compatibility with C-h and C-i in insert mode

--- config/init-key-bindings.el
+++ config/init-key-bindings.el
@@ -17,6 +17,8 @@
 (define-key evil-insert-state-map (kbd "C-p") 'file-fuzzy-finder)
 (define-key evil-insert-state-map (kbd "<tab>") 'my-company-tab)
 (define-key evil-insert-state-map (kbd "<backtab>") 'evil-shift-left-line)
+(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char)
+(define-key evil-insert-state-map (kbd "C-i") 'tab-to-tab-stop)
 (define-key evil-normal-state-map [escape] 'keyboard-quit)
 (define-key evil-visual-state-map [escape] 'keyboard-quit)
 (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
