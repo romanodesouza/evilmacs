553f3a31706aba1cb646af80d9c6d420af7f794a
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-13 14:13:14 -0200

    Multiple cursors extended

--- config/init-multiple-cursors.el
+++ config/init-multiple-cursors.el
@@ -3,6 +3,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-,") 'mc/mark-all-like-this-dwim)
+(global-set-key (kbd "C-;") 'mc/mark-more-like-this-extended)
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
 (provide 'init-multiple-cursors)
