abec85d0bba457b9a66354bfc3954dba3ba6f90f
Romano Augusto <romanoaugusto88@gmail.com>
2015-01-18 11:42:45 -0200

    Add more flexibility on nginx mode

--- config/init-nginx-mode.el
+++ config/init-nginx-mode.el
@@ -1,4 +1,5 @@
 (require-package 'nginx-mode)
-(add-to-list 'auto-mode-alist '("/etc/nginx/sites-available/.*" . nginx-mode))
+(add-to-list 'auto-mode-alist '("nginx.conf" . nginx-mode))
+(add-to-list 'auto-mode-alist '("nginx/sites-available/.*" . nginx-mode))
 
 (provide 'init-nginx-mode)
