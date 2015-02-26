3567758a3b75e0414d57f86b46656ab22539843b
Romano Augusto <romanoaugusto88@gmail.com>
2015-02-26 17:10:44 -0300

    Improve flycheck error reporting on saving buffer

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -2,6 +2,22 @@
 (require 'flycheck)
 
 (setq flycheck-idle-change-delay 0.2)
+
+(defun add-flycheck-list-hook()
+  (add-hook 'flycheck-after-syntax-check-hook #'flycheck-list-errors-only-when-errors))
+
+(defun remove-flycheck-list-hook()
+  (remove-hook 'flycheck-after-syntax-check-hook #'flycheck-list-errors-only-when-errors))
+
+(defun flycheck-list-errors-only-when-errors ()
+  (remove-flycheck-list-hook)
+  (if flycheck-current-errors
+    (flycheck-list-errors)
+    (-when-let (buffer (get-buffer flycheck-error-list-buffer))
+               (dolist (window (get-buffer-window-list buffer))
+                 (quit-window nil window)))))
+
+(add-hook 'before-save-hook #'add-flycheck-list-hook)
 (global-flycheck-mode t)
 
 (provide 'init-flycheck)
